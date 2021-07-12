0d11e62feb3fe658e973d51d182e0735459b67b9
Jan-Wijbrand Kolman <jw@n--tree.net>
2021-07-12 22:10:26 +0200

    revert back to "document" API

--- tests/entry_point.py
+++ tests/entry_point.py
@@ -3,14 +3,12 @@
 
 import logging
 
-import orthanc as runtime  # NOQA provided by the plugin runtime.
+import orthanc  # NOQA provided by the plugin runtime.
 
 from orthanc_ext import event_dispatcher
-from orthanc_ext import orthanc
 from orthanc_ext.logging_configurator import orthanc_logging
 
-
-orthanc_logging(runtime)
+orthanc_logging(orthanc)
 
 
 def log_event(event, _):
@@ -30,12 +28,8 @@ def stop_handler(event, _):
     logging.info(f'Orthanc stop handler')
 
 
-registry = event_dispatcher.Registry(runtime)
+registry = event_dispatcher.Registry(orthanc)
 registry.add_handler(event_dispatcher.ANY, log_event)
-registry.add_handler(
-    runtime.ChangeType.ORTHANC_STARTED,
-    start_maintenance_cycle, show_system_info)
-registry.add_handler(
-    runtime.ChangeType.ORTHANC_STOPPED,
-    stop_handler)
+registry.add_handler(orthanc.ChangeType.ORTHANC_STARTED, start_maintenance_cycle, show_system_info)
+registry.add_handler(orthanc.ChangeType.ORTHANC_STOPPED, stop_handler)
 registry.bind()
